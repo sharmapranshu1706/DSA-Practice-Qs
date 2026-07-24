04d549ad8a8a9c9920c8585bc37d6a1a863a6893
Pranshu Sharma <sharma.pranshu1706@gmail.com>
2026-07-25 00:12:33 +0530

    Array first Qs

---
+++ Arrays Qs/src/indexReturn.java
@@ -0,0 +1,14 @@
+public static int indexValueReurn(int[] arr, int i) {
+    if (i >= 0 && i < arr.length) {
+        return arr[i];
+    }
+    throw new IllegalArgumentException("Index out of bounds. " +
+            "Please provide a valid index between 0 and " + (arr.length - 1) + ".");
+}
+
+void main() {
+    int[] arr = {24, 25, 26, 27, 28};
+    int i = 4;
+    int element = indexValueReurn(arr, i);
+    IO.println("Element at index " + i + " is: " + element);
+}
