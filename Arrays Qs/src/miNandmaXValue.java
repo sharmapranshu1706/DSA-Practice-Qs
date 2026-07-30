d2d00202c33fbd74e806a5eba791420d059aa61f
Pranshu Sharma <sharma.pranshu1706@gmail.com>
2026-07-30 08:22:46 +0530

    Minimum and Maximum Value From int Array (Add Enhanced Loop & Some Parameters changed

--- Arrays Qs/src/miNandmaXValue.java
+++ Arrays Qs/src/miNandmaXValue.java
@@ -3,19 +3,19 @@ public class miNandmaXValue {
         int maxValue = Integer.MIN_VALUE;
         int minValue = Integer.MAX_VALUE;
 
-        for (int i = 0; i < number.length; i++) {
+        for (int n : number) {
 
-            if (number[i] > maxValue) {
-                maxValue = number[i];
+            if (n > maxValue) {
+                maxValue = n;
             }
-            if (number[i] < minValue) {
-                minValue = number[i];
+            if (n < minValue) {
+                minValue = n;
             }
         }
         System.out.println("Maximum value is: " + maxValue);
         System.out.println("Minimum value is: " + minValue);
     }
-    public static void main(String[] args){
+    static void main(String[] a){
         int[] number = {55, 22, 33, 44, 11};
         miNandmaXValue obj = new miNandmaXValue();
         obj.findmiNmaXValue(number);
