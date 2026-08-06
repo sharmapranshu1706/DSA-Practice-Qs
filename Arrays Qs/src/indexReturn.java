90afddd74028933f9bdbf2015ffc978922b5799f
Pranshu Sharma <sharma.pranshu1706@gmail.com>
2026-08-07 00:00:13 +0530

    06/Aug inedxReturn Problem

--- Arrays Qs/src/indexReturn.java
+++ Arrays Qs/src/indexReturn.java
@@ -1,14 +1,13 @@
-public static int indexValueReturn(int[] arr, int i) {
-    if (i >= 0 && i < arr.length) {
-        return arr[i];
+public class indexReturn{
+    public int indexValueReturn(int[] arr, int i){
+        if(i>=0 && i<arr.length){
+            return arr[i];
+        }
+        throw new IllegalArgumentException("Index out of bounds");
     }
-    throw new IllegalArgumentException("Index out of bounds. " +
-            "Please provide a valid index between 0 and " + (arr.length - 1) + ".");
-}
-
-void main() {
-    int[] arr = {24, 25, 26, 27, 28};
-    int i = 4;
-    int element = indexValueReturn(arr, i);
-    IO.println("Element at index " + i + " is: " + element);
+    public static void main(String[] args){
+        indexReturn n = new indexReturn();
+        int sol = n.indexValueReturn(new int[]{1,2,3,4,5}, 2);
+        System.out.println(sol);
+    }
 }
